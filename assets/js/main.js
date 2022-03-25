4f34cb19d4894d64cfbe173ac11a7bcc0be04852
MCigno94 <mattiacigno27@gmail.com>
2022-03-25 18:37:07 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,26 @@
 const kmRate = 0.21;
 
 // Chiedere il nome del passeggero
-const yourName = parseInt(prompt("Qual è il tuo nome?"));
+const yourName = prompt("Qual è il tuo nome?");
+document.querySelector(".yourName").value = yourName;
+
 // Chiedere il numero di km da pecorrere
 const kmTraveled = parseInt(prompt("Quanti km vuoi percorrere?"));
+document.querySelector(".kmTraveled").value = kmTraveled;
 // Chiedere l'età del passeggero
-const yourAge = parseInt(prompt("Quanti anni hai?"));
+const yourAge = parseInt(prompt("Inserisci l'età"));
 // impostare tariffa in € per il chilometraggio
 const kmPrice = kmTraveled * kmRate;
 
 // tariffa del biglietto
-let ticketPrice = "";
+let ticketPrice = 0;
+let message = "";
 // applico uno sconto del 20% se minorenne
 const priceMinors = kmPrice * 0.8;
 // applico uno sconto del 40% se over65
 const priceOver65 = kmPrice * 0.6;
+
+
 
 // - impostare i limiti della variabile "kmTraveled" - nessuna stringa e nessun numero minore di 0
 if (kmTraveled < 0 || isNaN(kmTraveled)) {
@@ -30,12 +36,35 @@ if (yourAge < 0 || yourAge > 100 || isNaN(yourAge)) {
 if (yourAge < 18) {
     // applico uno sconto del 20% se minorenne
     ticketPrice = priceMinors;
+    message = "minorenne";
+    document.querySelector(".form-select").value = 1;
 } else if (yourAge >= 65) {
     // applico uno sconto del 40% se over65
     ticketPrice = priceOver65;
+    message = "over65";
+    document.querySelector(".form-select").value = 3;
 } else {
     // prezzo pieno
     ticketPrice = kmPrice;
+    message = "maggiorenne";
+    document.querySelector(".form-select").value = 2;
 }
 
-//document.getElementById("ticket_price").innerHTML = ticketPrice.toFixed(2) + "€";
+// inserisco dati nel biglietto
+document.querySelector(".fullName").innerHTML = yourName;
+
+document.querySelector(".ageSelect").innerHTML = message;
+
+document.querySelector(".ticketPrice").innerHTML = ticketPrice + "€";
+
+// genera biglietto
+const elementTicketMain = document.querySelector('.ticketMain');
+const elementGeneraButton = document.querySelector('.genera');
+const elementAnnullaButton = document.querySelector('.annulla');
+
+elementGeneraButton.addEventListener('click', function() {
+    elementTicketMain.classList.remove('ticketOff');
+});
+elementannullaButton.addEventListener('click', function() {
+    elementTicketMain.classList.add('ticketOff');
+});
